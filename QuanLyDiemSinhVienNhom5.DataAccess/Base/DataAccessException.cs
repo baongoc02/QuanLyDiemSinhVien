70d848252cb96638e2dc12f4ae64878559204395
ngthvan1612 <ngthvan1612@gmail.com>
2022-11-12 17:36:17 +0700

    Create DataAccessException.cs

---
+++ QuanLyDiemSinhVienNhom5.DataAccess/Base/DataAccessException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyDiemSinhVienNhom5.DataAccess.Base
+{
+    public class DataAccessException : Exception
+    {
+        public DataAccessException(string message)
+            : base(message)
+        {
+
+        }
+    }
+}
