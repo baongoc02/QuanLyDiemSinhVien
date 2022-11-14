6cf51b1e1e3b9016ac088074cd4eb8876d477cef
baongoc02 <tranthibaongoc779152@gmail.com>
2022-11-14 14:46:50 +0700

    thêm entity  DSKetQuaHocTapByMSSV và DSKetQuaHocTapByMSSVViewModel

---
+++ QuanLyDiemSinhVienNhom5.Core/ViewModel/DSKetQuaHocTapByMSSVViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyDiemSinhVienNhom5.Core.ViewModel
+{
+    public class DSKetQuaHocTapByMSSVViewModel
+    {
+        public string TenSinhVien { get; set; }
+        public string MaLop {get; set; }
+        public string TenMonHoc { get; set; }
+        public float DiemTrungBinh { get; set; }
+        public string Loai { get; set; }
+
+        public DSKetQuaHocTapByMSSVViewModel()
+        {
+
+        }
+    }
+}
---
+++ QuanLyDiemSinhVienNhom5.DataAccess/DAO/DSKetQuaHocTapByMaSinhVien.cs
@@ -0,0 +1,23 @@
+﻿using QuanLyDiemSinhVienNhom5.DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyDiemSinhVienNhom5.DataAccess.DAO
+{
+    public class DSKetQuaHocTapByMaSinhVien
+    {
+        public string TenSinhVien { get; set; }
+        public string MaLop { get; set; }
+        public string TenMonHoc { get; set; }
+        public float DiemTrungBinh { get; set; }
+        public string Loai {get; set; }
+
+        public DSKetQuaHocTapByMaSinhVien()
+        {
+
+        }
+    }
+}
