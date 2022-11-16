96b0d4aefa9b353f5fd7f8cb213a92d202cf37b4
ngthvan1612 <ngthvan1612@gmail.com>
2022-11-16 10:44:53 +0700

    Cập nhật thêm kết quả học tập DAO
    - Danh sách sv không đạt môn nào đó
    - Kết quả học tập theo lớp và xếp loại
    - Kết quả học tập theo sinh viên
    - Tính stc và điểm trung bình

---
+++ QuanLyDiemSinhVienNhom5.DataAccess/Model/DSSVKhongDatMonHoc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyDiemSinhVienNhom5.DataAccess.Model
+{
+    public class DSSVKhongDatMonHoc
+    {
+        public string MaSinhVien { get; set; }
+        public string MaLop { get; set; }
+        public string TenMon { get; set; }
+        public double DiemTrungBinh { get; set; }
+        public string TenHocKy { get; set; }
+        public string TenNamHoc { get; set; }
+    }
+}
