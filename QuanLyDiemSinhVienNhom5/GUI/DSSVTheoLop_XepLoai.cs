bca88ab2f6c7836fd617e5bfb6c31a54098a35ba
baongoc02 <tranthibaongoc779152@gmail.com>
2022-11-16 21:13:53 +0700

    thêm xử lí form lấy danh sách theo lớp và xếp loại
    - nút tìm
    - load gridview
    - form _ load

--- QuanLyDiemSinhVienNhom5/GUI/DSSVTheoLop_XepLoai.cs
+++ QuanLyDiemSinhVienNhom5/GUI/DSSVTheoLop_XepLoai.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using QuanLyDiemSinhVienNhom5.Core.Services;
+using QuanLyDiemSinhVienNhom5.Core.ViewModel;
+using QuanLyDiemSinhVienNhom5.DataAccess.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -17,15 +20,41 @@ namespace QuanLyDiemSinhVienNhom5.GUI
             InitializeComponent();
         }
 
+        public void LoadDSSinhVien(IEnumerable<KetQuaHocTapTheoMaLopVaXepLoaiViewModel> ketQuaHocTapTheoMaLopVaXepLoaiViewModels)
+        {
+            DSSV_gridview.Columns.Clear();
+            DSSV_gridview.DataSource = ketQuaHocTapTheoMaLopVaXepLoaiViewModels;
+
+            txtSoLuong.Text = DSSV_gridview.RowCount.ToString();
+        }
+
         private void Btn_Tim_Click(object sender, EventArgs e)
         {
-
+            KetQuaHocTapService ketQuaHocTapService = new KetQuaHocTapService();
+            List<KetQuaHocTapTheoMaLopVaXepLoaiViewModel> ketQuaHocTapTheoMaLopVaXepLoaiViewModels = ketQuaHocTapService.GetKetQuaHocTapTheoMaLopVaXepLoai(cbLop.SelectedValue.ToString(), cbXepLoai.SelectedItem.ToString());
+            LoadDSSinhVien(ketQuaHocTapTheoMaLopVaXepLoaiViewModels);
         }
 
         [DesignOnly(true)]
         private void DSSVTheoLop_XepLoai_Load(object sender, EventArgs e)
         {
+            string[] xepLoais = new string[] { "Xuất sắc", "Giỏi", "Khá", "Trung bình", "Yếu", "Kém" };
+            cbXepLoai.Items.AddRange(xepLoais);
 
+            LopService lopService = new LopService();
+            var listLop = lopService.ListAll();
+            foreach (var lop in listLop)
+            {
+                lop.TenLop = lop.MaLop;
+            }
+            listLop.Insert(0, new LopViewModel()
+            {
+                MaLop = "",
+                TenLop = "-- Tất cả các lớp --"
+            });
+            cbLop.DataSource = listLop;
+            cbLop.DisplayMember = nameof(LopViewModel.TenLop);
+            cbLop.ValueMember = nameof(LopViewModel.MaLop);
         }
 
         private void DSSV_gridview_CellClick(object sender, DataGridViewCellEventArgs e)
