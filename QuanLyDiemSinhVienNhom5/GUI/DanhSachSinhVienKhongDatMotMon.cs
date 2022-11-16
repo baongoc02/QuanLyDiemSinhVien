3f0251534e8ddaaeccaad9c296a6c46e167a1e6f
baongoc02 <tranthibaongoc779152@gmail.com>
2022-11-16 21:13:03 +0700

    thêm xử lí form lấy danh sách sinh viên không đạt một môn
    - nút tìm
    - load gridview
    - form _ load

--- QuanLyDiemSinhVienNhom5/GUI/DanhSachSinhVienKhongDatMotMon.cs
+++ QuanLyDiemSinhVienNhom5/GUI/DanhSachSinhVienKhongDatMotMon.cs
@@ -1,4 +1,5 @@
 ﻿using QuanLyDiemSinhVienNhom5.Core.Services;
+using QuanLyDiemSinhVienNhom5.Core.ViewModel;
 using QuanLyDiemSinhVienNhom5.DataAccess.ViewModel;
 using System;
 using System.Collections.Generic;
@@ -21,19 +22,37 @@ namespace QuanLyDiemSinhVienNhom5.GUI
         }
 
 
-        private void DSSV_gridview_CellClick(object sender, DataGridViewCellEventArgs e)
+        public void LoadDSSVKhongDatMotMon(IEnumerable<DSSVKhongDatMonHocViewModel> dSSVKhongDatMonHocViewModels)
         {
+            DSSV_gridview.Columns.Clear();
+            DSSV_gridview.DataSource = dSSVKhongDatMonHocViewModels;
+
+
+            txtSoLuong.Text =  DSSV_gridview.RowCount.ToString();
 
         }
 
         private void Btn_Tim_Click(object sender, EventArgs e)
         {
-
+            KetQuaHocTapService ketQuaHocTapService = new KetQuaHocTapService();
+            List<DSSVKhongDatMonHocViewModel> dSSVKhongDatMonHocViewModels = ketQuaHocTapService.GetDSSVKhongDatMonHoc(cbMon.SelectedValue.ToString(), cbHocKy.SelectedValue.ToString());
+            LoadDSSVKhongDatMotMon(dSSVKhongDatMonHocViewModels);
         }
 
         [DesignOnly(true)]
         private void DanhSachSinhVienKhongDatMotMon_Load(object sender, EventArgs e)
         {
+            MonHocService monHocService = new MonHocService();
+            var listMonHoc = monHocService.ListAll();
+            listMonHoc.Insert(0, new MonHocViewModel()
+            {
+                MaMonHoc = "",
+                TenMonHoc = "-- Tất cả môn học --"
+            });
+            cbMon.DataSource = listMonHoc;
+            cbMon.DisplayMember = nameof(MonHocViewModel.TenMonHoc);
+            cbMon.ValueMember = nameof(MonHocViewModel.MaMonHoc);
+
             HocKyService hocKyService = new HocKyService();
             var listHocKy = hocKyService.ListAll();
             listHocKy.Insert(0, new HocKyViewModel()
