3edfbdf9378ed3e25c41d0d849357c71873a334a
ngthvan1612 <ngthvan1612@gmail.com>
2022-11-16 11:12:08 +0700

    Create DSSVKhongDatMonHocViewModel.cs

---
+++ QuanLyDiemSinhVienNhom5.Core/ViewModel/DSSVKhongDatMonHocViewModel.cs
@@ -0,0 +1,33 @@
+﻿using QuanLyDiemSinhVienNhom5.DataAccess.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyDiemSinhVienNhom5.Core.ViewModel
+{
+    public class DSSVKhongDatMonHocViewModel
+    {
+        [DisplayName("Mã sinh viên")]
+        public string MaSinhVien { get; set; }
+
+        [DisplayName("Họ tên")]
+        public string HoTen { get; set; }
+
+        [DisplayName("Điểm trung bình")]
+        public double DiemTrungBinh { get; set; }
+
+        [DisplayName("Tên khoa")]
+        public string Khoa { get; set; }
+
+        public DSSVKhongDatMonHocViewModel(DSSVKhongDatMonHoc model)
+        {
+            this.MaSinhVien = model.MaSinhVien;
+            this.DiemTrungBinh = model.DiemTrungBinh;
+            this.Khoa = model.Khoa;
+            this.HoTen = model.HoTen;
+        }
+    }
+}
