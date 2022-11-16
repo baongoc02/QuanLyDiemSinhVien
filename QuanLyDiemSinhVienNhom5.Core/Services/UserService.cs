f6b71b2c7bcdcf67b4243870a0d497f5dae924ea
baongoc02 <tranthibaongoc779152@gmail.com>
2022-11-16 16:52:29 +0700

    thêm userService

---
+++ QuanLyDiemSinhVienNhom5.Core/Services/UserService.cs
@@ -0,0 +1,41 @@
+﻿using QuanLyDiemSinhVienNhom5.DataAccess.Base;
+using QuanLyDiemSinhVienNhom5.DataAccess.DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyDiemSinhVienNhom5.Core.Services
+{
+    public class UserService : BaseService
+    {
+        private readonly UserDAO userDAO;
+
+        public UserService()
+        {
+            this.userDAO = new UserDAO();
+        }
+
+        public int IsAdmin(string userName)
+        {
+            try
+            {
+                bool result = this.userDAO.CheckRole(userName, "sysadmin");
+                if (result == true) return 1;
+                else return 2;
+            }
+            catch (DataAccessException e)
+            {
+                this.OnError(e.Message);
+                return -1;
+            }
+            catch (Exception e)
+            {
+                this.OnError("Lỗi hệ thống");
+                this.OnError(e.Message);
+                return -1;
+            }
+        }
+    }
+}
