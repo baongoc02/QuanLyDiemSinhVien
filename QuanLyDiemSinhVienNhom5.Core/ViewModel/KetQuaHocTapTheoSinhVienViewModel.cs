9ccc508bc9b35cbefab7c98cba117e276ee9b0c3
ngthvan1612 <ngthvan1612@gmail.com>
2022-11-16 10:45:44 +0700

    Thêm View Model cho các kết quả học tập
    - Danh sách sv không đạt môn nào đó
    - Kết quả học tập theo lớp và xếp loại
    - Kết quả học tập theo sinh viên
    - Tính stc và điểm trung bìn

---
+++ QuanLyDiemSinhVienNhom5.Core/ViewModel/KetQuaHocTapTheoSinhVienViewModel.cs
@@ -0,0 +1,41 @@
+﻿using QuanLyDiemSinhVienNhom5.DataAccess.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyDiemSinhVienNhom5.Core.ViewModel
+{
+    public class KetQuaHocTapTheoSinhVienViewModel
+    {
+        [DisplayName("Mã sinh viên")]
+        public string MaSinhVien { get; set; }
+
+        [DisplayName("Tên sinh viên")]
+        public string TenSinhVien { get; set; }
+
+        [DisplayName("Mã lớp")]
+        public string MaLop { get; set; }
+
+        [DisplayName("Tên môn học")]
+        public string TenMonHoc { get; set; }
+
+        [DisplayName("Điểm trung bình")]
+        public double DiemTrungBinh { get; set; }
+
+        [DisplayName("Loại")]
+        public string Loai { get; set; }
+
+        public KetQuaHocTapTheoSinhVienViewModel(KetQuaHocTapTheoSinhVien model)
+        {
+            this.MaSinhVien = model.MaSinhVien;
+            this.TenSinhVien = model.TenSinhVien;
+            this.MaLop = model.MaLop;
+            this.TenMonHoc = model.TenMonHoc;
+            this.DiemTrungBinh = model.DiemTrungBinh;
+            this.Loai = model.Loai;
+        }
+    }
+}
---
+++ QuanLyDiemSinhVienNhom5.Core/ViewModel/TinhSTCAndDiemTrungBinhViewModel.cs
@@ -0,0 +1,37 @@
+﻿using QuanLyDiemSinhVienNhom5.DataAccess.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyDiemSinhVienNhom5.Core.ViewModel
+{
+    public class TinhSTCAndDiemTrungBinhViewModel
+    {
+        [DisplayName("Tên sinh viên")]
+        public string TenSinhVien { get; set; }
+
+        [DisplayName("Mã lớp")]
+        public string MaLop { get; set; }
+
+        [DisplayName("Tên môn học")]
+        public string TenMonHoc { get; set; }
+
+        [DisplayName("Điểm trung bình")]
+        public double DiemTrungBinh { get; set; }
+
+        [DisplayName("Loại")]
+        public string Loai { get; set; }
+
+        public TinhSTCAndDiemTrungBinhViewModel(TinhSTCAndDiemTrungBinh model)
+        {
+            this.TenSinhVien = model.TenSinhVien;
+            this.MaLop = model.MaLop;
+            this.TenMonHoc = model.TenMonHoc;
+            this.DiemTrungBinh = model.DiemTrungBinh;
+            this.Loai = model.Loai;
+        }
+    }
+}
