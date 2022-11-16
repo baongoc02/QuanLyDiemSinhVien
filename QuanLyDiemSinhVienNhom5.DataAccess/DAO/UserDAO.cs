db57f75543359c7044071cf46e09f57f0ff2d919
baongoc02 <tranthibaongoc779152@gmail.com>
2022-11-16 21:07:42 +0700

    thêm UserDAO

---
+++ QuanLyDiemSinhVienNhom5.DataAccess/DAO/UserDAO.cs
@@ -0,0 +1,33 @@
+﻿using QuanLyDiemSinhVienNhom5.DataAccess.SqlServer;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyDiemSinhVienNhom5.DataAccess.DAO
+{
+    public class UserDAO : BaseDAO
+    {
+        public bool CheckRole(string username, string roleName)
+        {
+            var conn = SqlServerConnectionSingleon.getInstance();
+            using (var command = conn.CreateCommand())
+            {
+                try
+                {
+                    command.CommandText = "SELECT dbo.Func_Check_Role(@userName, @roleName)";
+                    command.Parameters.Add(new SqlParameter("@userName", username));
+                    command.Parameters.Add(new SqlParameter("@roleName", roleName));
+                    return Convert.ToInt32(command.ExecuteScalar()) == 1;
+                }
+                catch (Exception e)
+                {
+                    base.ProcessSqlException(e);
+                    return false;
+                }
+            }
+        }
+    }
+}
